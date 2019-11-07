af959d0b9cbd9b442b52c66cc040b6669885ade9
Tom Nguyen <huuthong103@gmail.com>
2019-11-06 23:48:48 -0800

    developing and learning new stuffs

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 var game;
+
+// Frame per second(speed)
 var FPS = 60;
 var speed = function(fps){
     FPS = parseInt(fps);
 }
 
+// Load Images
 var loadImages = function(src, callback){
     var new_img = 0;
     var loaded = 0;
@@ -23,6 +26,7 @@ var loadImages = function(src, callback){
     console.log("images loaded")
 }
 
+// Bird class
 var Bird = function(json){
     this.x = 80;
     this.y = 250;
@@ -52,25 +56,81 @@ Bird.prototype.update = function(){
 Bird.prototype.flap = function(){
     this.gravity = this.jump;
 }
+//** End Of Bird Class */
 
+//** Pipe Class */
+var Pipe = function(json){
+    this.x = 0;
+    this.y = 0;
+    this.width = 50;
+    this.height = 40;
+    this.speed = 3;
+
+    this.init(json);
+}
+
+Pipe.prototype.init = function(json){
+    for(var i in json){
+        this[i] = json[i];
+    }
+}
+
+Pipe.prototype.update = function(){
+    this.x -= this.speed;
+}
+
+Pipe.prototype.isOut = function(){
+    if(this.x + this.width < 0){
+        return true;
+    }
+}
+//** End of Pipe Class */
+
+
+//** Game Class */
 var Game = function(){
     this.birds = [];
+    this.pipes = [];
     this.canvas = document.querySelector('#flappy');
     this.ctx = this.canvas.getContext("2d");
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+    this.interval = 0;
+    this.generation = 0;
+    this.alives = 0;
+
     this.backgroundX = 0;
     this.backgroundSpeed = 0.5;
-
 }
 
 Game.prototype.start = function(){
-    
+    this.interval = 0;
+    this.pipes = [];
+    this.birds = [];
+
+    this.gen = Neuvol.nextGeneration();
+    // Store birds into this.birds using nextGeneration method in Neuvol(Neuroevolution)
+    for(var i in this.gen){
+        var b = new Bird();
+        this.birds.push(b);
+    }
+
+    this.generation++; // increase the generation
+    this.alives = this.birds.length; // how many birds alive
 }
 
 Game.prototype.update = function(){
     this.backgroundX += this.backgroundSpeed;
 
+}
+
+Game.prototype.isEnd = function(){
+    for(var i in this.birds){
+        if(this.birds[i].alive){
+            return false;
+        }
+    }
+    return true;
 }
 
 Game.prototype.display = function(){
@@ -93,6 +153,10 @@ window.onload = function(){
         pipbottom: "./img/pipebottom.png",
     }
     var start_game = function(){
+        Neuvol = new Neuroevolution({
+            population: 100,
+            network: [2,[2],1],
+        });
         game = new Game();
         game.start();
         game.update();
